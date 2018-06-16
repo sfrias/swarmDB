bceb0abc0a32dbd5393f8c9d5a40139abe6d0c0b
isabel <isabel@izabyl.com>
2018-06-15 18:26:04 -0700

    pbft sometimes works! Now to figure out why it segfaults

--- scripts/pbft-client/main.py
+++ scripts/pbft-client/main.py
@@ -3,16 +3,25 @@ from websocket import create_connection
 from google.protobuf import text_format
 import pbft_pb2
 import json
+import sys
 
-request = pbft_pb2.pbft_request()
-request.client = "cli client"
-request.timestamp = 1
-request.operation = "iunno, something"
-encoded_request = text_format.MessageToString(request)
-wrapped_msg = json.dumps({"bzn-api": "pbft", "pbft-data": encoded_request})
+timestamp = 1
 
-print(wrapped_msg)
+for line in sys.stdin:
+    request = pbft_pb2.pbft_request()
+    request.client = "cli client"
+    request.timestamp = timestamp
+    timestamp += 1
+    request.operation = line.strip()
 
-ws = create_connection("ws://127.0.0.1:50000")
-ws.send(wrapped_msg)
-ws.close()
+    message = pbft_pb2.pbft_msg()
+    message.request.CopyFrom(request)
+    message.type = pbft_pb2.PBFT_MSG_TYPE_REQUEST
+
+    encoded_message = text_format.MessageToString(message)
+    wrapped_message = json.dumps({"bzn-api": "pbft", "pbft-data": encoded_message})
+
+    ws = create_connection("ws://127.0.0.1:50000")
+    print("Sending message: ", wrapped_message)
+    ws.send(wrapped_message)
+    ws.close()
